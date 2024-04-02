b326e27bccec7b32e031ec0bbbc60815dd3e074c
ShubhamK-S <shubhamkumarsingh528@gmail.com>
2024-04-02 23:28:35 +0530

    Updated motor controls

--- FollowBot/FollowBot.ino
+++ FollowBot/FollowBot.ino
@@ -1,4 +1,3 @@
-//include the library code:
 #include<NewPing.h>           
 #include<Servo.h>             
 #include<AFMotor.h>           
@@ -19,6 +18,8 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);  //NewPing setup of pins and
 //create motor objects
 AF_DCMotor Motor1(1,MOTOR12_1KHZ);
 AF_DCMotor Motor2(2,MOTOR12_1KHZ);
+AF_DCMotor Motor3(3,MOTOR34_1KHZ);
+AF_DCMotor Motor4(4,MOTOR34_1KHZ);
 
  Servo myservo; //create servo object to control the servo:
  int pos=0;     //variable to store the servo position:
@@ -27,7 +28,7 @@ void setup() { // the setup function runs only once when power on the board or r
   
    Serial.begin(9600); //initailize serial communication at 9600 bits per second:
    myservo.attach(10); // servo attached to pin 10 of Arduino UNO
-
+{
 for(pos = 90; pos <= 180; pos += 1){    // goes from 90 degrees to 180 degrees:
   myservo.write(pos);                   //tell servo to move according to the value of 'pos' variable:
   delay(15);                            //wait 15ms for the servo to reach the position:
@@ -40,16 +41,17 @@ for(pos = 0; pos<=90; pos += 1) {       //goes from 180 degrees to 0 degrees:
   myservo.write(pos);                   //tell servo to move according to the value of 'pos' variable:
   delay(15);                            //wait 15ms for the servo to reach the position:
   }
+}
    pinMode(RIGHT, INPUT); //set analog pin RIGHT as an input:
    pinMode(LEFT, INPUT);  //set analog pin RIGHT as an input:
 }
 
-// the loop function runs forever
+// the lope function runs forever
 void loop() {                             
   
 delay(50);                                        //wait 50ms between pings:
 distance = sonar.ping_cm();                       //send ping, get distance in cm and store it in 'distance' variable:
-Serial.print("distance:");                   
+Serial.print("distance");                   
 Serial.println(distance);                         // print the distance in serial monitor:
 
 
@@ -61,38 +63,54 @@ Serial.println(Right_Value);                      // print the right IR sensor v
 Serial.print("LEFT");                       
 Serial.println(Left_Value);                       //print the left IR sensor value in serial monitor:
 
-if((distance > 3) && (distance < 15)){            //check wheather the ultrasonic sensor's value stays between 1 to 15.
+if((distance > 3) && (distance < 10)){            //check wheather the ultrasonic sensor's value stays between 1 to 15.
                                                   //If the condition is 'true' then the statement below will execute:
   //Move Forward:
-  Motor1.setSpeed(130);  //define motor1 speed:
+  Motor1.setSpeed(150);  //define motor1 speed:
   Motor1.run(FORWARD);   //rotate motor1 clockwise:
-  Motor2.setSpeed(130);  //define motor2 speed:
+  Motor2.setSpeed(150);  //define motor2 speed:
   Motor2.run(FORWARD);   //rotate motor2 clockwise:
+  Motor3.setSpeed(150);  //define motor3 speed:
+  Motor3.run(FORWARD);   //rotate motor3 clockwise:
+  Motor4.setSpeed(150);  //define motor4 speed:
+  Motor4.run(FORWARD);   //rotate motor4 clockwise:
   
 }else if((Right_Value==0) && (Left_Value==1)) {   //If the condition is 'true' then the statement below will execute:
   
   //Turn Left                                                
-  Motor1.setSpeed(150);  //define motor1 speed:
+  Motor1.setSpeed(190);  //define motor1 speed:
   Motor1.run(FORWARD);   //rotate motor1 cloclwise:
-  Motor2.setSpeed(150);  //define motor2 speed:
+  Motor2.setSpeed(190);  //define motor2 speed:
   Motor2.run(FORWARD);   //rotate motor2 clockwise:
-  delay(150);
+  Motor3.setSpeed(190);  //define motor3 speed:
+  Motor3.run(BACKWARD);  //rotate motor3 anticlockwise:
+  Motor4.setSpeed(190);  //define motor4 speed:
+  Motor4.run(BACKWARD);  //rotate motor4 anticlockwise:
+  delay(50);
   
 }else if((Right_Value==1)&&(Left_Value==0)) {     //If the condition is 'true' then the statement below will execute:
   
   //Turn Right
-  Motor1.setSpeed(150);  //define motor1 speed:
+  Motor1.setSpeed(190);  //define motor1 speed:
   Motor1.run(BACKWARD);  //rotate motor1 anticlockwise:
-  Motor2.setSpeed(150);  //define motor2 speed:
+  Motor2.setSpeed(190);  //define motor2 speed:
   Motor2.run(BACKWARD);  //rotate motor2 anticlockwise:
-  delay(150);
+  Motor3.setSpeed(190);  //define motor3 speed:
+  Motor3.run(FORWARD);   //rotate motor3 clockwise:
+  Motor4.setSpeed(190);  //define motor4 speed:
+  Motor4.run(FORWARD);   //rotate motor4 clockwise:
+  delay(50);
   
-}else if(distance > 15) {                          //If the condition is 'true' then the statement below will execute:
+}else if(distance > 10) {                          //If the condition is 'true' then the statement below will execute:
   
   //Stop
   Motor1.setSpeed(0);    //define motor1 speed:
   Motor1.run(RELEASE);   //stop motor1:
   Motor2.setSpeed(0);    //define motor2 speed:
   Motor2.run(RELEASE);   //stop motor2:
+  Motor3.setSpeed(0);    //define motor3 speed:
+  Motor3.run(RELEASE);   //stop motor3:
+  Motor4.setSpeed(0);    //define motor4 speed:
+  Motor4.run(RELEASE);   //stop motor4:
 }
 }
